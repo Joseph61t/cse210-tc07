3dfc6523e42d01c9718d3fd9a69e014a8858e87d
Ninja1672 <powellhunter1997@gmail.com>
2021-06-08 11:32:51 -0600

    Words worked

--- speed/speed/speed_template/speed/game/dictionary.py
+++ speed/speed/speed_template/speed/game/dictionary.py
@@ -4,17 +4,18 @@ from typing import Text
 class Dictionary:
 
     def __init__(self):
-        self._words = self.load_words()
+        self._words = []
+        self.load_words()
         
     # reads the file and adds it to a list, to be only called in the constrcutor  
     # for some reason the method doens't recognize the words.txt file right now
     # so it doesn't work   
     def load_words(self):
-        filename = 'words.txt'
-        with open(filename) as file:
+        
+        with open("speed\speed\speed_template\speed\game\dict.txt") as file:
             for line in file:
                 self._words.append(line)
-                print('done loading words')
+            print('done loading words')
 
     # returns a random word from the list
     def get_word(self):
@@ -23,6 +24,5 @@ class Dictionary:
 
 
 
-dictionary = Dictionary()
-dictionary.get_word()
-
+# dictionary = Dictionary()
+# print(dictionary.get_word())

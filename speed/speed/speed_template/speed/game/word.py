f3e810614b129ff4c85536c0ca40a8542a26086c
Ninja1672 <powellhunter1997@gmail.com>
2021-06-08 12:01:17 -0600

    fixed word

--- speed/speed/speed_template/speed/game/word.py
+++ speed/speed/speed_template/speed/game/word.py
@@ -15,12 +15,7 @@ class Word(Actor):
         self.set_velocity(velocity)
 
     def get_word(self):
-
-<<<<<<< HEAD
-        self.velocity = random.randint(1,10)
-=======
         return self.word
->>>>>>> 2e4c1c00bba281b16b5110a269242ac11f8784a0
 
     def set_word(self, word):
         self.word = word
